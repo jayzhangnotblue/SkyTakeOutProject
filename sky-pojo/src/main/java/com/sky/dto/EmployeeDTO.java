0c3b2b6f3256ac55dd9c006e4a8159b68c5bae8e
jayzhang <2651740639@qq.com>
2024-10-25 21:22:05 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,26 @@
 package com.sky.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "新增员工传入进来的数据")
 public class EmployeeDTO implements Serializable {
-
+    @ApiModelProperty("ID")
     private Long id;
-
+    @ApiModelProperty("用户名/账号")
     private String username;
-
+    @ApiModelProperty("姓名")
     private String name;
-
+    @ApiModelProperty("电话号码")
     private String phone;
-
+    @ApiModelProperty("性别")
     private String sex;
-
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
 }
